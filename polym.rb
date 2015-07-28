c6a0f06f773283f2d727ec502dff09ed86f19473
kamutu <wangechililian@gmail.com>
2015-07-28 11:46:32 +0300

    first commit

---
+++ polym.rb
@@ -0,0 +1,16 @@
+class Dog
+def run
+print'i am running'
+end
+def bark
+print 'i am barking'
+end
+end
+newDog = Dog.new
+newDog.bark
+class Wildog < Dog
+def run
+print 'wohoo'
+end 
+end
+#tryong polymorphism dont know wht it isnt going thru advise
